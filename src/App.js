5d413bdf22341b85b7bb5130afbf37dc1de97030
Prince Allwin <princeallwin@Princes-MacBook-Air.local>
2023-03-12 14:51:10 +0530

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { Router } from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
     return (
         <div className="app">
             {/* Header */}
             <Header />
-            {/* Home */}
-            <Home />
+            <Routes>
+                {/* Home */}
+                <Route path="/" element={<Home />} />
+                <Route path="/checkout" element={<h1>Checkout Page</h1>} />
+            </Routes>
         </div>
     );
 }
